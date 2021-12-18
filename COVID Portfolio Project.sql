739f51e979a8064277b9d6862d2ee2f869357931
Anatoly <55873602+Iamanatoliy@users.noreply.github.com>
2021-12-18 21:54:00 +0300

    Add files via upload

---
+++ COVID Portfolio Project.sql
@@ -0,0 +1,155 @@
+/* Covid 19 Data Exploration // Исследование данных COVID 19
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+/*
+
+
+select * 
+from PortfolioProject..CovidDeaths
+Where continent is not null 
+order by 3,4
+
+-- Select Data that we are going to be starting with // Выберем данные, с которыми мы будем работать
+
+
+select location, date, total_cases, new_cases, total_deaths, population
+from PortfolioProject..CovidDeaths
+Where continent is not null 
+order by 1,2
+
+
+-- Total Cases vs Total Deaths // Общее количество случаев заболевания по сравнению с общим количеством смертей
+-- Shows the probability of death in case of covid disease infection in Russia (my location) // Показывает вероятность смерти в случае заражения ковидными заболеваниями в России (мое местоположение)
+
+
+select location, date, total_cases, total_deaths, (CAST((total_deaths) AS Float) / total_cases) * 100 AS DeathPercentage
+from PortfolioProject..CovidDeaths
+WHERE location like '%russia'
+and continent is not null 
+order by 1,2
+
+--total cases vs population // Общее количество случаев по сравнению с населением
+--Shows what percentage of population infected with Covid // Показывает, какой процент населения инфицирован Covid
+
+select location, date, population, total_cases, total_deaths, (CAST((total_cases) AS Float) / population) * 100 AS PercentagePopulationInfected
+from PortfolioProject..CovidDeaths
+WHERE location like '%russia'
+order by 1,2
+
+-- Countries with Highest Infection Rate compared to Population // Страны с самым высоким уровнем инфицирования по сравнению с населением
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((CAST((total_cases) AS Float)/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--WHERE location like '%russia'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- Countries with Highest Death Count per Population // Страны с самым высоким показателем смертности на душу населения
+
+Select Location, MAX(cast(Total_deaths as float)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%russia%'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+
+-- BREAKING THINGS DOWN BY CONTINENT // РАЗДЕЛЕНИЕ ПО КОНТИНЕНТАМ 
+-- Showing contintents with the highest death count per population // Показываем континенты с наибольшим количеством смертей на численность населения
+
+Select continent, MAX(cast(Total_deaths as float)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%russia%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+
+-- GLOBAL NUMBERS // ГЛОБАЛЬНЫЕ ПОКАЗАТЕЛИ
+
+Select SUM(cast(new_cases as float)) as total_cases, SUM(cast(new_deaths as float)) as total_deaths, SUM(cast(new_deaths as float))/SUM(cast(new_cases as float))*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%russia%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+-- Total Population vs Vaccinations // Общее население и количество привитых
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine // Показывает процент населения, которое получило хотя бы одну вакцину Ковид
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+
+-- Using CTE to perform Calculation on Partition By in previous query // Используя CTE для выполнения вычисления по Partition By в предыдущем запросе
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+From PopvsVac
+
+
+
+-- Using Temp Table to perform Calculation on Partition By in previous query // Используя таблицу Temp Table для расчета по Partition By в предыдущем запросе
+
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+--where dea.continent is not null 
+--order by 2,3
+
+Select *, (RollingPeopleVaccinated/Population)*100
+From #PercentPopulationVaccinated
+
+
+
+ --Creating View to store data for later visualizations // Создаем представление для хранения данных для последующей визуализации
+
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+
